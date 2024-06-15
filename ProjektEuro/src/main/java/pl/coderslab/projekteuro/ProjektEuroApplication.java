66ddf788fe4e5fe25534ac9d6167aaf86172f218
Franciszek  T <franciszektatar@gmail.com>
2024-06-15 11:14:32 +0200

    pierwszy commit

---
+++ ProjektEuro/src/main/java/pl/coderslab/projekteuro/ProjektEuroApplication.java
@@ -0,0 +1,13 @@
+package pl.coderslab.projekteuro;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjektEuroApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ProjektEuroApplication.class, args);
+    }
+
+}
---
+++ ProjektEuro/src/test/java/pl/coderslab/projekteuro/ProjektEuroApplicationTests.java
@@ -0,0 +1,13 @@
+package pl.coderslab.projekteuro;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjektEuroApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
